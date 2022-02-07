89b2c13d825a12c4d086999e6a584305afa756de
unverzed <vitoriarortega@gmail.com>
2022-02-07 12:49:01 -0300

    inserindo as cartas no jogo

--- scripts.js
+++ scripts.js
@@ -1,6 +1,47 @@
-//pergunta quantas cartas o jogador quer e valida se é um número par de 4 a 14
+let cardsGame = parseInt(prompt("Quantas cartas você deseja? (números pares de 4 a 14"));
+const cards = [
+    './images/akechi.gif',
+    './images/ann.gif',
+    './images/futaba.gif',
+    './images/joker.gif',
+    './images/makoto.gif',
+    './images/ryuji.gif',
+    './images/yusuke.gif'
+]
+function random() {
+    return Math.random() - 0.5;
 
-let cards = prompt("Quantas cartas você deseja? (números pares de 4 a 14");
-while (cards % 2 !== 0 || cards > 14 || cards < 4) {
-    cards = prompt("Insira um número válido");
 }
+
+//pergunta quantas cartas o jogador quer e valida se é um número par de 4 a 14
+function allCards () {
+    while (cardsGame % 2 !== 0 || cardsGame > 14 || cardsGame < 4) {
+    cardsGame = prompt("Insira um número válido");
+}}
+allCards();
+
+//função que retorna a quantidade de cartas que o jogador informou anteriormente
+function gameStart() {
+    let cardsInGame = [];
+    for (let i = 0; i < cardsGame / 2; i++) {
+      cardsInGame.push(cards[i]);
+      cardsInGame.push(cards[i]);
+    }
+  
+    cardsInGame.sort(random);
+  
+    const include = document.querySelector(".all-cards");
+    for (let x = 0; x < cardsInGame.length; x++) {
+      include.innerHTML =
+        include.innerHTML +
+        ` <article class="card" onclick="" data-identifier="card">
+        <img src="./images/plogo.png" class="img-card front-face face" data-identifier="front-face" />
+         <img src="${cardsInGame[x]}" class="img-card back-face face" data-identifier="back-face"/ >
+     </article>`;
+    }
+  }
+  gameStart();
+
+  function clickCard() {
+    
+  }
